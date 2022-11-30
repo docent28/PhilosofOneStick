3ffe3b216ab4bd906a18d5681da2b25e9f4b393f
docent28 <eaa67@mail.ru>
2022-11-30 16:11:54 +0300

    Вводим любое количество философов Выбираем случайных философов при каждой итерации

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,8 +5,26 @@ fun main(args: Array<String>) {
     val philosophers = ArrayList<Philisopher>()
     val sticks = ArrayList<Stick>()
 
+    for (i in 0 until countPhilosopher) {
+        print("Имя философа номер $i - ")
+        val namePhilosopher = readln()
+        philosophers.add(Philisopher(namePhilosopher))
+        sticks.add(Stick("stick$i"))
+    }
 
+    val timeMap: MutableMap<Int, String> =
+        philosophers.associateBy(keySelector = { philosophers.indexOf(it) },
+            valueTransform = { it.name }
+        ) as MutableMap<Int, String>
 
+    while (timeMap.isNotEmpty()) {
+        val indexPhilosopher = selectPhilosopherRandom(timeMap)
+    }
+
+}
+
+fun selectPhilosopherRandom(mapPhilosopher: Map<Int, String>): Int {
+    return ArrayList(mapPhilosopher.keys).random()
 }
 
 fun enteringCountNum(): Int {
